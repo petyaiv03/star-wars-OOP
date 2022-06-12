ed2ba2a529f76a548e7bf5e29f29b37c35792f0e
Petya <petyaivs03@gmail.com>
2022-06-12 20:31:11 +0300

    created class Galaxy

--- galaxy.h
+++ galaxy.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "planet.h"
+#include "string.h"
+class Galaxy {
+private:
+	Planet* planets;
+	int size;
+public:
+	Galaxy();
+	Galaxy(const Galaxy& other);
+	Galaxy& operator = (const Galaxy& other);
+	~Galaxy();
+private:
+	void copy(const Galaxy& other);
+	void clear();
+};
