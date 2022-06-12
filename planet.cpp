9e447bac388e5dbb50e510b98603ebcd9ec1eb9c
Petya <petyaivs03@gmail.com>
2022-06-12 20:23:06 +0300

    added class planet

---
+++ planet.cpp
@@ -0,0 +1,43 @@
+#include "planet.h"
+
+Planet::Planet()
+{
+	planetName = nullptr;
+	jediOnPlanet = nullptr;
+	amountOfJedi = 0;
+}
+
+Planet::Planet(const Planet& other)
+{
+	copy(other);
+}
+
+Planet& Planet::operator=(const Planet& other)
+{
+	if (this != &other)
+	{
+		clear();
+		this->copy(other);
+	}
+	return *this;
+}
+
+Planet::~Planet()
+{
+	clear();
+}
+
+void Planet::copy(const Planet& other)
+{
+	planetName = other.planetName;
+	for (int i = 0; i < amountOfJedi; ++i)
+	{
+		jediOnPlanet[i] = other.jediOnPlanet[i];
+	}
+	amountOfJedi = other.amountOfJedi;
+}
+
+void Planet::clear()
+{
+	delete[] jediOnPlanet;
+}
--- planet.h
+++ planet.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "string.h"
+#include "jedi.h"
+
+class Planet {
+private:
+	String planetName;
+	Jedi* jediOnPlanet;
+	int amountOfJedi;
+public:
+	Planet();
+	Planet(const Planet& other);
+	Planet& operator=(const Planet& other);
+	~Planet();
+private:
+	void copy(const Planet& other);
+	void clear();
+};
